2e14cc9f5bee79d27818c1f5c02ece041ec4bd8b
wayfzbee <96890594+wayfzbee@users.noreply.github.com>
2022-04-06 18:19:30 +0000

    Amount strings to always include all 8 decimals. Never use exponential notation.

--- tests.py
+++ tests.py
@@ -33,12 +33,12 @@ class SecretWebcashTestCase(unittest.TestCase):
         amount = 500
         secret_value = "feedbeef"
         n1 = SecretWebcash(amount, secret_value)
-        self.assertEqual(str(n1), f"e{amount}:secret:{secret_value}")
+        self.assertEqual(str(n1), f"e500.00000000:secret:{secret_value}")
 
     def test_bank_webcash_repr(self):
         amount = 120
         n1 = SecretWebcash(amount, "feedbeef")
-        self.assertEqual(repr(n1), f"SecretWebcash(amount=\"{amount}\", secret_value=\"feedbeef\")")
+        self.assertEqual(repr(n1), f"SecretWebcash(amount=\"120.00000000\", secret_value=\"feedbeef\")")
 
     def test_amounts(self):
         count = 12
@@ -53,6 +53,33 @@ class SecretWebcashTestCase(unittest.TestCase):
         assert all([webcashes[x].amount == amounts[x] for x in range(0, len(amounts))])
         assert all([SecretWebcash.deserialize(str(webcash)) == webcash for webcash in webcashes])
         assert all([SecretWebcash.deserialize(str(webcash)).amount == webcash.amount for webcash in webcashes])
+
+        expectations = [
+            { "val": Decimal("1E-8"),
+              "in":  "e1E-8:secret:feedbeef",
+              "out": "e0.00000001:secret:feedbeef"
+            },
+            { "val": Decimal("1E-8"),
+              "in":  "e0.00000001:secret:feedbeef",
+              "out": "e0.00000001:secret:feedbeef"
+            },
+            { "val": Decimal("1E-6"),
+              "in":  "e1E-6:secret:feedbeef",
+              "out": "e0.00000100:secret:feedbeef"
+            },
+            { "val": Decimal("1E-6"),
+              "in":  "e0.00000100:secret:feedbeef",
+              "out": "e0.00000100:secret:feedbeef"
+            },
+            { "val": Decimal("100.001"),
+              "in":  "e100.00100000:secret:feedbeef",
+              "out": "e100.00100000:secret:feedbeef"
+            },
+        ];
+        for exp in expectations:
+            swc = SecretWebcash.deserialize(exp["in"])
+            self.assertEqual(str(swc), exp["out"])
+            self.assertEqual(swc.amount, exp["val"])
 
     def test_invalid_amounts(self):
         amount = Decimal("0.123456789") # too many decimals
